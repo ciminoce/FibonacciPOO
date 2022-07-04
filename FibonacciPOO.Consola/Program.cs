e2848784786db0514caa935167639b8d55846047
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-07-04 19:40:11 -0300

    Agregar archivos de proyecto.

---
+++ FibonacciPOO.Consola/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FibonacciPOO.Entidades;
+
+namespace FibonacciPOO.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var fibonacci = new Fibonacci();
+                if (fibonacci.Validar())
+                {
+                    Console.WriteLine(fibonacci.MostrarSerie());
+                    var index = 5;
+                    Console.WriteLine(fibonacci.GetTermino(index));
+
+                    Console.WriteLine(fibonacci.MostrarTerminosDecreciente());
+                    var posicion = fibonacci.GetPosicion(1);
+                    Console.WriteLine($"1 se encuentra en la posicion {posicion}");
+                    var cantidad = fibonacci.GetCantidadQueSeRepite(1);
+                    Console.WriteLine($"1 aparece {cantidad} veces");
+                }
+                else
+                {
+                    Console.WriteLine("Objeto no válido...");
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FibonacciPOO.Entidades/Fibonacci.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+namespace FibonacciPOO.Entidades
+{
+    public class Fibonacci
+    {
+        private int n;
+        private int[] terminos;
+        
+        public int GetN => n;
+
+        public Fibonacci():this(10)
+        {
+            //n = 10;
+        }
+        public Fibonacci(int cantidad)
+        {
+            n = cantidad;
+        }
+
+        public bool Validar()
+        {
+            return n >= 2;
+        }
+
+        private void GenerarTerminos()
+        {
+            terminos=new int[n];
+            terminos[0] = 0;
+            terminos[1] = 1;
+            for (int i = 2; i < terminos.Length; i++)
+            {
+                terminos[i] = terminos[i - 2] + terminos[i - 1];
+            }
+
+        }
+
+        public string MostrarSerie()
+        {
+            GenerarTerminos();
+            StringBuilder sb=new StringBuilder();
+            foreach (var termino in terminos)
+            {
+                sb.Append($"{termino} ");
+            }
+
+            return sb.ToString();
+        }
+
+        public int GetPosicion(int termino)
+        {
+            return Array.IndexOf(terminos, termino);
+        }
+        public int GetTermino(int posicion)
+        {
+            if (posicion<0 || posicion>=n)
+            {
+                throw new Exception("Posición fuera del rango");
+            }
+
+            return terminos[posicion];
+        }
+
+        public string MostrarTerminosDecreciente()
+        {
+            var terminosCopia = new int[n];
+            Array.Copy(terminos,terminosCopia,terminos.Length);
+            Array.Reverse(terminosCopia);
+            return this.MostrarSerie(terminosCopia);
+        }
+
+        public string MostrarSerie(int[]array)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var termino in array)
+            {
+                sb.Append($"{termino} ");
+            }
+
+            return sb.ToString();
+        }
+
+        public int GetCantidadQueSeRepite(int termino)
+        {
+            var cantidad = Array.FindAll(terminos, n=>n==termino);
+            return cantidad.Length;
+        }
+    }
+}
